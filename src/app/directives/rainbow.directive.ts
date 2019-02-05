74e23f451da123529ce99aaf84ba3935db0751db
rsignes <romain.signes@gmail.com>
2019-02-05 10:37:52 +0100

    complex directive

--- src/app/directives/rainbow.directive.ts
+++ src/app/directives/rainbow.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Poney } from './../interfaces/poney';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[scaRainbow]'
@@ -6,13 +7,30 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class RainbowDirective {
 
   elementService: ElementRef
-  @Input() color: string
+  @Input('scaRainbow') color: string
+  @Input() isColored: boolean
+  @Input() poney: Poney
 
   constructor(elementRef: ElementRef) {
     this.elementService = elementRef
   }
 
   ngOnInit() {
-    this.elementService.nativeElement.style.backgroundColor = this.color
+    if (this.isColored) {
+      this.elementService.nativeElement.style.backgroundColor = this.color
+    }
+  }
+
+  @HostListener('dblclick') handleDblclick() {
+    // Faire avancer le poney de 5 de distance
+    this.poney.distance += 5
+    // afficher son arc-en-ciel pendant 1s
+    let rainbowImg = this.poney.img.replace('running', 'rainbow')
+    this.poney.img = rainbowImg
+
+    setTimeout(() => {
+      let runningImg = this.poney.img.replace('rainbow', 'running')
+      this.poney.img = runningImg
+    }, 1000)
   }
 }
