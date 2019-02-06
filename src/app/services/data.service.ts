55261c6efd46abdf799ec1cdc9bd59827d3f9f00
rsignes <romain.signes@gmail.com>
2019-02-06 09:45:13 +0100

    data service optimization

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Race } from '../interfaces/race';
 import { Poney } from '../interfaces/poney';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators'
 
 @Injectable({
@@ -15,13 +15,34 @@ export class DataService {
   API_URL: string = 'http://localhost:3000'
 
   get ponies(): Observable<Poney[]> {
-    return this.http.get(`${this.API_URL}/ponies`)
-    .pipe(map(ponies => <Poney[]>ponies))
+    // return ponies if already requested
+    return (this._ponies && this._ponies.length) ?
+      of(this._ponies) :
+      this.http.get(`${this.API_URL}/ponies`)
+        .pipe(map(ponies => <Poney[]>ponies))
+        .pipe(map(ponies => this._ponies = ponies))
   }
 
   get races(): Observable<Race[]> {
-    return this.http.get(`${this.API_URL}/races`)
-    .pipe(map(races => <Race[]>races))
+    let obs$: Observable<Race[]>
+
+    if (this._races && this._races.length) {
+      obs$ = of(this._races)
+      
+      // new Observable((observer) => {
+      //   observer.next(this._races)
+      // })
+    } else {
+      obs$ = this.http.get(`${this.API_URL}/races`)
+      .pipe(map(races => {
+        // save races
+        this._races = <Race[]>races
+
+        return <Race[]>races
+      }))
+    }
+
+    return obs$
   }
 
   getRaceById(id: string): Observable<Race> {
